e452902fc707feb79c0c47c979245f66618b9c77
incev <ilyaka89@gmail.com>
2023-08-21 01:17:15 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,69 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.example.exceptions.WrongLoginException;
+import org.example.exceptions.WrongPasswordException;
+
 public class Main {
+
+    private static final String VALIDATE_PATTERN = "^[a-zA-z0-9_]+$";
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        validate("login", "pass", "pass");
+        validate("loginloginloginloginloginloginloginloginlogin", "pass", "pass");
+        validate("login", "pass", "pass2");
+        validate("login", "passpasspasspasspasspasspasspasspasspasspasspasspasspass", "pass");
+        validate("login%%", "pass", "pass");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
+
+    public static Boolean validate(String login, String password, String confirmPassword) {
+
+        boolean isValid = true;
+
+        try {
+            validateLogin(login);
+            validatePassword(password, confirmPassword);
+
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка с введенным логином" + e.getMessage());
+            isValid = false;
+
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка с введенным логином" + e.getMessage());
+            isValid = false;
+
+        }
+
+        if (isValid) {
+            System.out.println("Логин и пароль корректны");
+        }
+
+        return isValid;
+    }
+
+    public static void validateLogin(String login) throws WrongLoginException {
+        if (!login.matches(VALIDATE_PATTERN)) {
+            throw new WrongLoginException("Логин может содержать в себе только латинские буквы, цифры и знак подчеркивания.");
+        }
+        if (login.length() > 20) {
+            throw new WrongLoginException("Логин не может быть больше 20 символов.");
+        }
+
+    }
+
+    public static void validatePassword(String password, String confirmPassword) throws WrongPasswordException {
+        if (!password.matches(VALIDATE_PATTERN)) {
+            throw new WrongPasswordException("Пароль может содержать в себе только латинские буквы, цифры и знак подчеркивания.");
+        }
+        if (password.length() > 20) {
+            throw new WrongPasswordException("ПАроль не может быть больше 20 символов.");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают.");
+        }
+
+    }
+
+
 }
